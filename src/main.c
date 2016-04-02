17dc73546fc838b93e453ba44e6592a7e45e248e
Evan Klitzke <evan@eklitzke.org>
2016-04-02 01:51:58 -0700

    correct copyright year

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-// Copyright Evan Klitzke <evan@eklitzke.org>, 2015
+// Copyright Evan Klitzke <evan@eklitzke.org>, 2016
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
